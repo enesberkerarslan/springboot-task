b686d4bb3a54a289a574426e514d5d26773c1fbe
Enes Berk Erarslan <63202386+enesberkerarslan@users.noreply.github.com>
2024-02-02 19:46:28 +0300

    first commit

---
+++ src/main/java/com/JavaRestfulApi/springboottask/SpringbootTaskApplication.java
@@ -0,0 +1,14 @@
+package com.JavaRestfulApi.springboottask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTaskApplication.class, args);
+		int a = 0;
+	}
+
+}
---
+++ src/test/java/com/JavaRestfulApi/springboottask/SpringbootTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaRestfulApi.springboottask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
